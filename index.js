cdbf1e2661c58263fc1a562a630d23c38a88aca7
mustafa-mehboob <57095619+mustafa-mehboob@users.noreply.github.com>
2020-12-06 18:34:21 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
+var name = prompt ("What is your name");
+console.log(name);
+
 var age = 18;
-var feepaid = "ghulam mustafa";
+var feepaid = " WELCOME ";
 var  nationality;
  nationality = "uk";
-alert(feepaid);
- 
+
+// we will ignore this line
+// console.log(nationality); 
 console.log(age);
 age = 40;
 console.log(age);
 
- var a;
- a = 5;
- var b;
- b = 6;
+ var a = 6;
+ ++a; // result 7
+ var b = 6; 
+ --b; // result 5
  var c ;
- c = a + b;
- console.log(c);
+ c = a++ - ++b - ++a // answer will be -8
+ 
+ console.log(c); // Addition  
 
  var d;
- console.log(d);
+ console.log(d); // this will display undefined
 
  var e = null;
- console.log(e);
+ console.log(e); 
 
  var chng = 34;
  console.log(chng);
@@ -28,3 +33,22 @@ console.log(age);
  console.log(chng);
  chng = true;
  console.log(chng);
+
+var f = 5;
+var g = 10;
+var h = f < g && g > 6;    // And (&&) Operator
+console.log(h);            
+var i = h > 6 && f < h;    // And  (&&) Operator
+console.log(h);
+
+var j = 10;
+var k = 7;
+var l = k > j || k < 8;  // Not (||) Operator
+console.log(l);    
+var m = k > j || k < 2;  // Not (||) Operator
+console.log(m);      
+
+var n = 2;
+var o = !(a < 4);       // Or ! Operator
+console.log(o);
+
